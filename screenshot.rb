3407635a2638e28a3b9a238fa3e4bfdfe6723106
Chris Masters <chrismasters@gmail.com>
2014-11-06 22:09:21 +0100

    Working with fast rgb

--- screenshot.rb
+++ screenshot.rb
@@ -6,28 +6,23 @@ url = ARGV[0].to_s
 trimmed_url = url.sub("https://twitter.com/", "")
 username_tweet_id = trimmed_url.split("/status/").join("_")
 
-width = 1400
-height = 768
-
 driver = Selenium::WebDriver.for :firefox 
 driver.get url
 driver.manage.window.maximize
 
-# wait = Selenium::WebDriver::Wait.new(:timeout => 10) # seconds
-# wait.until {
-#   tweet.displayed?
-#   tweet_media.displayed? if tweet_media
-# }
-
-# improve this with callbacks
-sleep 20
-
 tweet = driver.find_element(:xpath, "//div[@role='main']")
-# tweet_media = driver.find_element(:css, "div.js-media-container")
+
+wait = Selenium::WebDriver::Wait.new(:timeout => 10) # seconds
+wait.until {
+  tweet.displayed?
+}
+
+driver.execute_script('return arguments[0].style.transformOrigin = "top center"', tweet)
+driver.execute_script('return arguments[0].style.transform = "scale(2)"', tweet)
+driver.execute_script('return arguments[0].style.zIndex = "200000000"', tweet)
 
 driver.save_screenshot("./tmp/twitter.png")
 
 full_image = ChunkyPNG::Image.from_file("./tmp/twitter.png")
-
-full_image.crop!(tweet.location.x + 1, tweet.location.y + 1, tweet.size.width - 2, tweet.size.height - 5)
-full_image.save("./screenshots/#{username_tweet_id}.png")
+full_image.crop!(tweet.location.x + 10, tweet.location.y + 2, tweet.size.width - 10, tweet.size.height - 5)
+full_image.save("./screenshots/#{username_tweet_id}.png", :fast_rgb)
