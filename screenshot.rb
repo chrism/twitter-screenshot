c713fae630c007b13bfdec6f0f04c324341c74e0
Chris Masters <chrismasters@gmail.com>
2014-11-07 12:15:31 +0100

    Cleans up browser with rescue.

--- screenshot.rb
+++ screenshot.rb
@@ -8,29 +8,33 @@ url = ARGV[0].to_s
 trimmed_url = url.sub("https://twitter.com/", "")
 username_tweet_id = trimmed_url.split("/status/").join("_")
 
-driver = Selenium::WebDriver.for :firefox 
-driver.get url
-driver.manage.window.maximize
+begin
+  driver = Selenium::WebDriver.for :firefox 
+  driver.get url
+  driver.manage.window.maximize
 
-tweet = driver.find_element(:xpath, "//div[@role='main']")
+  tweet = driver.find_element(:xpath, "//div[@role='main']")
 
-wait = Selenium::WebDriver::Wait.new(:timeout => 10) # seconds
-wait.until {
-  tweet.displayed?
-}
+  wait = Selenium::WebDriver::Wait.new(:timeout => 10) # seconds
+  wait.until {
+    tweet.displayed?
+  }
 
-if driver.find_elements(:css, "button.display-this-media").length > 0
-  driver.find_element(:css, "button.display-this-media").click
+  if driver.find_elements(:css, "button.display-this-media").length > 0
+    driver.find_element(:css, "button.display-this-media").click
+  end
+
+  driver.execute_script('return arguments[0].style.transformOrigin = "top center"', tweet)
+  driver.execute_script('return arguments[0].style.transform = "scale(2)"', tweet)
+  driver.execute_script('return arguments[0].style.zIndex = "200000000"', tweet)
+
+  driver.save_screenshot("./tmp/twitter.png")
+
+  full_image = ChunkyPNG::Image.from_file("./tmp/twitter.png")
+  full_image.crop!(tweet.location.x + 10, tweet.location.y + 2, tweet.size.width - 20, tweet.size.height - 5)
+  full_image.save("./screenshots/#{username_tweet_id}.png", :fast_rgb)
+rescue
+  puts "there was an error :("
+ensure
+  driver.quit
 end
-
-driver.execute_script('return arguments[0].style.transformOrigin = "top center"', tweet)
-driver.execute_script('return arguments[0].style.transform = "scale(2)"', tweet)
-driver.execute_script('return arguments[0].style.zIndex = "200000000"', tweet)
-
-driver.save_screenshot("./tmp/twitter.png")
-
-full_image = ChunkyPNG::Image.from_file("./tmp/twitter.png")
-full_image.crop!(tweet.location.x + 10, tweet.location.y + 2, tweet.size.width - 20, tweet.size.height - 5)
-full_image.save("./screenshots/#{username_tweet_id}.png", :fast_rgb)
-
-driver.quit
