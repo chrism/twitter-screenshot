972797ef6ad60b64a5f28a9a7411e3f429adebe7
Chris Masters <chrismasters@gmail.com>
2016-06-13 13:14:22 +0200

    Final commit for archiving

--- screenshot.rb
+++ screenshot.rb
@@ -4,6 +4,7 @@ require 'selenium-webdriver'
 require 'oily_png'
 
 url = ARGV[0].to_s
+comments = ARGV[1].to_s
 
 trimmed_url = url.sub("https://twitter.com/", "")
 username_tweet_id = trimmed_url.split("/status/").join("_")
@@ -13,7 +14,13 @@ begin
   driver.get url
   driver.manage.window.maximize
 
-  tweet = driver.find_element(:xpath, "//div[@role='main']")
+  if comments == "--with-comments"
+    puts "grabbing with comments"
+    tweet = driver.find_element(:xpath, "//div[@role='main']")
+  else
+    puts "grabbing without comments add flag --with-comments if you want them"
+    tweet = driver.find_element(:css, "div.permalink-tweet-container")
+  end
 
   wait = Selenium::WebDriver::Wait.new(:timeout => 10) # seconds
   wait.until {
@@ -25,7 +32,7 @@ begin
   end
 
   driver.execute_script('return arguments[0].style.transformOrigin = "top center"', tweet)
-  driver.execute_script('return arguments[0].style.transform = "scale(2)"', tweet)
+  # driver.execute_script('return arguments[0].style.transform = "scale(2)"', tweet)
   driver.execute_script('return arguments[0].style.zIndex = "200000000"', tweet)
 
   driver.save_screenshot("./tmp/twitter.png")
@@ -33,6 +40,7 @@ begin
   full_image = ChunkyPNG::Image.from_file("./tmp/twitter.png")
   full_image.crop!(tweet.location.x + 10, tweet.location.y + 2, tweet.size.width - 20, tweet.size.height - 5)
   full_image.save("./screenshots/#{username_tweet_id}.png", :fast_rgb)
+  puts "tweet was grabbed :)"
 rescue
   puts "there was an error :("
 ensure
